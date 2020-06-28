6c18699dd30a2d2d01430e3a31db407fb3b19d77
Cem Freimoser <cem.freimoser@zuehlke.com>
2020-06-28 02:06:30 +0200

    First impl. of --all-namespace recreation for ingress objects

--- cmd/ingressCmd.go
+++ cmd/ingressCmd.go
@@ -6,6 +6,7 @@ import (
 	"kube-recreate/pkg/k8s"
 	"kube-recreate/pkg/util"
 
+	kv1 "k8s.io/api/core/v1"
 	v1beta1 "k8s.io/api/networking/v1beta1"
 
 	"github.com/spf13/cobra"
@@ -27,10 +28,14 @@ func NewIngressCommand(rCmd *ingressCmd) *cobra.Command {
 			rCmd.ns = getNamespace(genericclioptions.NewConfigFlags(true), c)
 
 			if getAllFlag(c) {
-				return rCmd.run("")
+				return rCmd.run("", false)
 			}
 			if len(args) == 1 {
-				return rCmd.run(args[0])
+				return rCmd.run(args[0], false)
+			}
+			if getAllNamespacesFlag(c) {
+				return rCmd.run("", true)
+
 			}
 
 			return errors.New("Define Resource or use --all")
@@ -40,20 +45,33 @@ func NewIngressCommand(rCmd *ingressCmd) *cobra.Command {
 	return cmd
 }
 
-func (ir *ingressCmd) run(name string) error {
+func (ir *ingressCmd) run(name string, allNamespaces bool) error {
 	client, err := k8s.NewK8sClient()
 	if err != nil {
 		return err
 	}
 
 	var ingresses []v1beta1.Ingress
+	var namespaces []string
 	if len(name) == 0 {
-		l, err := client.LsIngress(ir.ns)
-		if err != nil {
-			return err
+		if allNamespaces {
+			nsList, err := client.LsNamespaces()
+			if err != nil {
+				return err
+			}
+			namespaces = append(namespaces, mapNamespacesToNames(nsList)...)
+		} else {
+			namespaces = append(namespaces, ir.ns)
 		}
 
-		ingresses = append(ingresses, l...)
+		for _, ns := range namespaces {
+			l, err := client.LsIngress(ns)
+			if err != nil {
+				return err
+			}
+
+			ingresses = append(ingresses, l...)
+		}
 	} else {
 		i, err := client.GetIngress(ir.ns, name)
 		if err != nil {
@@ -90,4 +108,12 @@ func deleteAndRecreate(ingresses []v1beta1.Ingress, reporter *util.Reporter, cli
 
 		reporter.Append(i.Name, "Ingress", "CREATED", i.CreationTimestamp.String())
 	}
+}
+
+func mapNamespacesToNames(namespaces []kv1.Namespace) []string {
+	var result []string
+	for _, ns := range namespaces {
+		result = append(result, ns.Name)
+	}
+	return result
 }
--- pkg/k8s/client.go
+++ pkg/k8s/client.go
@@ -3,8 +3,10 @@ package k8s
 import (
 	"context"
 	// TODO new api version
+	kv1 "k8s.io/api/core/v1"
 	v1beta1 "k8s.io/api/networking/v1beta1"
 	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 )
@@ -69,4 +71,13 @@ func (client *K8sClient) CreateIngress(ingress *v1beta1.Ingress) (*v1beta1.Ingre
 	ctx := context.Background()
 	iclient := client.clientset.NetworkingV1beta1().Ingresses(ingress.Namespace)
 	return iclient.Create(ctx, ingress, v1.CreateOptions{})
+}
+
+func (client *K8sClient) LsNamespaces() ([]kv1.Namespace, error) {
+	ctx := context.Background()
+	list, err := client.clientset.CoreV1().Namespaces().List(ctx, v1.ListOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return list.Items, nil
 }
